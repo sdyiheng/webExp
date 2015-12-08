f2d8943da03238ba06036b8ce030fea829257630
dave <sdyiheng@gmail.com>
2015-12-08 16:14:13 +0800

    d

---
+++ backbone Demo/HtmlPage1.js
@@ -0,0 +1,22 @@
+﻿
+$(function () {
+    var User = Backbone.Model.extend({
+        name: 'xcd'
+    });
+
+    var ouser = new User();
+    alert(ouser.name);
+
+    var User2 = Backbone.Model.extend({
+        initialize: function (name) {
+            this.set({ name: name });
+        }
+    });
+    var user = new User2('刀狂剑痴');
+    alert(user.get('name'));
+
+    user.set({ name: 'ddd' });
+    alert(user.get('name'));
+
+})
+//  windows.alert('test');
