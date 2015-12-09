477b6dbd7fb539095e541a1428f4eba22b6cca9e
dave <sdyiheng@gmail.com>
2015-12-09 16:02:24 +0800

    print demo

---
+++ knockout Demo/clickCounter/app.js
@@ -0,0 +1,18 @@
+﻿
+var ClickCounterViewModel = function () {
+    this.numberOfClicks = ko.observable(0);
+
+    this.registerClick = function () {
+        this.numberOfClicks(this.numberOfClicks() + 1);
+    };
+
+    this.resetClicks = function () {
+        this.numberOfClicks(0);
+    };
+
+    this.hasClickedTooManyTimes = ko.pureComputed(function () {
+        return this.numberOfClicks() >= 3;
+    }, this);
+};
+
+ko.applyBindings(new ClickCounterViewModel());
---
+++ knockout Demo/demo1/demo1.js
@@ -0,0 +1,13 @@
+﻿// Here's my data model
+var ViewModel = function (first, last) {
+    this.firstName = ko.observable(first);
+    this.lastName = ko.observable(last);
+
+    this.fullName = ko.pureComputed(function () {
+        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, 
+        //because these get called when evaluating fullName.
+        return this.firstName() + " " + this.lastName();
+    }, this);
+};
+
+ko.applyBindings(new ViewModel("abc", "def")); // This makes Knockout get to work
